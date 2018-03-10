4abe3f63b0a5d37154ca092ba3437c933a1ab8d0
vijay khatri <vijaykhatri22@gmail.com>
2018-03-10 17:50:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,19 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var flag = 0;
+  for (var i = 0;i<cart.length;i++){
+    if( item == cart[i].itemName){
+      cart.splice[i,1]; 
+      flag=1;
+    }
+  }
+  if (flag == 0){
+    return "That item is not in your cart.";
+  }
+  else{
+    return cart;
+  }
 }
 
 function placeOrder(cardNumber) {
